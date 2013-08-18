4bd1184f8aff9d54f120548104e6dfbaa8d33b3a
Kyle Clegg <kyle@solutionreach.com>
2013-08-18 13:54:03 -0700

    Updated main.m with a little function to prevent logging of superflous assert macrs everywhere.

--- GrowingPains2/main.m
+++ GrowingPains2/main.m
@@ -10,9 +10,36 @@
 
 #import "GPAppDelegate.h"
 
+typedef int (*PYStdWriter)(void *, const char *, int);
+
+static PYStdWriter _oldStdWrite;
+
+int __pyStderrWrite(void *inFD, const char *buffer, int size)
+{
+  if ( strncmp(buffer, "AssertMacros: queueEntry", 24) == 0 ) {
+    return 0;
+  }
+  return _oldStdWrite(inFD, buffer, size);
+}
+
+void __iOS7B5CleanConsoleOutput(void)
+{
+  _oldStdWrite = stderr->_write;
+  stderr->_write = __pyStderrWrite;
+}
+
 int main(int argc, char *argv[])
 {
   @autoreleasepool {
-      return UIApplicationMain(argc, argv, nil, NSStringFromClass([GPAppDelegate class]));
+    __iOS7B5CleanConsoleOutput();
+    return UIApplicationMain(argc, argv, nil, NSStringFromClass([GPAppDelegate class]));
   }
 }
+
+// Replaced below with the above code to prevent the printing of the AssertMacros logs
+//int main(int argc, char *argv[])
+//{
+//  @autoreleasepool {
+//    return UIApplicationMain(argc, argv, nil, NSStringFromClass([GPAppDelegate class]));
+//  }
+//}
