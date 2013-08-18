3e7b7bd6d9144011b5c3726b2ea0718d6601fbe3
Kyle Clegg <kyle@solutionreach.com>
2013-08-18 00:51:34 -0700

    Updated layouts for 4 inch phone again, added new app icon.

---
+++ GrowingPains2/GPEntryCell.h
@@ -0,0 +1,13 @@
+//
+//  GPEntryCell.h
+//  GrowingPains2
+//
+//  Created by Kyle Clegg on 8/16/13.
+//  Copyright (c) 2013 Paperplate LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GPEntryCell : UICollectionViewCell
+
+@end
